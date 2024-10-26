6358a3aabead7bdf48ecde759d20d6ad41386d94
Joshua Estes <estesjn2020@gmail.com>
2024-10-26 17:57:10 -0400

    Adding IRQ handler

--- src/fpgaint.c
+++ src/fpgaint.c
@@ -1,37 +1,45 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/device.h>
+#include <linux/gpio.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
-#include <linux/sched.h>
-#include <linux/platform_device.h>
 #include <linux/io.h>
-#include <linux/of.h>
-#include <linux/init.h>
-#include <linux/device.h>
 #include <linux/ioport.h>
-#include <linux/wait.h>
+#include <linux/irq.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/sched.h>
 #include <linux/semaphore.h>
 #include <linux/spinlock_types.h>
 #include <linux/uaccess.h>
-#include <linux/irq.h>
-#include <linux/gpio.h>
+#include <linux/wait.h>
 
 MODULE_AUTHOR("Joshua Estes");
 MODULE_DESCRIPTION("FPGA Interrupt Driver");
 MODULE_VERSION("0.1");
 
-static int __init hello_2_init(void)
+irqreturn_t fpga_interrupt_handler(int irq, void *dev_id, struct pt_regs *regs)
 {
-	pr_info("Hello, world 2\n");
-	return 0;
+    pr_info("Interrupt\n");
+    return IRQ_HANDLED;
 }
 
-static void __exit hello_2_exit(void)
+static int __init irq_ex_init(void)
 {
-	pr_info("Goodbye, world 2\n");
+    pr_info("Requesting IRQ LN 72\n");
+
+    free_irq(72, NULL);
+    return request_irq(72, fpga_interrupt_handler, IRQF_TRIGGER_RISING, "fpga_interrupt", NULL);
 }
 
-module_init(hello_2_init);
-module_exit(hello_2_exit);
+static void __exit irq_ex_exit(void)
+{
+    pr_info("Freeing IRQ LN 72\n");
+    free_irq(72, NULL);
+}
+
+module_init(irq_ex_init);
+module_exit(request_irq);
 
 MODULE_LICENSE("GPL");
