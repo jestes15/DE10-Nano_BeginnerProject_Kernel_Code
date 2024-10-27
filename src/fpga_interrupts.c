7eb963bf3cff4c90fcd740bc73cc365c720142b8
Joshua Estes <estesjn2020@gmail.com>
2024-10-27 00:40:59 -0400

    Kernel Module Works

--- src/fpga_interrupts.c
+++ src/fpga_interrupts.c
@@ -21,49 +21,133 @@ MODULE_AUTHOR("Joshua Estes");
 MODULE_DESCRIPTION("FPGA Interrupt Driver");
 MODULE_VERSION("0.1");
 
-#define DEV_NAME "fpga_interrupts"
+#define DEVNAME "fpga_interrupts"
 
-static irq_handler_t __isr(int irq, void *dev_id)
+#define NUM_INTERRUPTS 1
+
+static DEFINE_SEMAPHORE(interrupt_mutex);
+static DEFINE_SPINLOCK(interrupt_flag_lock);
+
+static int irq_nums[NUM_INTERRUPTS] = {0};
+static uint32_t irq_status[2];
+
+static ssize_t irq_status_show(struct device * dev, struct device_attribute * attr, char * buf)
 {
-	printk(KERN_INFO DEV_NAME ": ISR\n");
-	return (irq_handler_t)IRQ_HANDLED;
+   int ret;
+   int i;
+   uint32_t mask;
+
+   spin_lock(&interrupt_flag_lock);
+
+   for (i = 0; i < NUM_INTERRUPTS; i++) {
+      if (i % 32 == 0) {
+         mask = 1;
+      }
+      buf[i] = (irq_status[i / 32] & mask) ? '1' : '0';
+      mask <<= 1;
+   }
+
+   irq_status[0] = 0;
+   irq_status[1] = 0;
+
+   spin_unlock(&interrupt_flag_lock);
+
+   buf[NUM_INTERRUPTS] = '\n';
+   buf[NUM_INTERRUPTS+1] = '\0';
+   ret = NUM_INTERRUPTS + 2;
+
+   return ret;
 }
 
-static int __test_int_driver_probe(struct platform_device *pdev)
+static ssize_t irq_status_store(struct device * dev, struct device_attribute * attr, const char * buf, size_t len)
 {
-	int irq_num = -1;
-
-	irq_num = platform_get_irq(pdev, 0);
-	printk(KERN_INFO DEV_NAME ": IRQ Number: %d\n", irq_num);
-	return request_irq(irq_num, (irq_handler_t)__isr, IRQF_TRIGGER_NONE, DEV_NAME, NULL);
+   return -EROFS;
 }
 
-static int __test_int_driver_remove(struct platform_device *pdev)
+static DEVICE_ATTR_RW(irq_status);
+
+static irq_handler_t __fpga_interrupts_driver_isr(int irq, void* kobj)
 {
-	int irq_num = -1;
+   int i;
 
-	irq_num = platform_get_irq(pdev, 0);
-	printk(KERN_INFO DEV_NAME ": Removing IRQ Number: %d\n", irq_num);
-	free_irq(irq_num, NULL);
+   for (i = 0; i < NUM_INTERRUPTS; i++) {
+      if (irq_nums[i] == irq) {
+         break;
+      }
+   }
+   if (i == NUM_INTERRUPTS) {
+      return (irq_handler_t) IRQ_NONE; 
+   }
 
-	return 0;
+   spin_lock(&interrupt_flag_lock);
+
+   irq_status[i / 32] |= (1 << (i % 32));
+
+   spin_unlock(&interrupt_flag_lock);
+
+   sysfs_notify((struct kobject *) kobj, NULL, "irq_status");
+   
+   return (irq_handler_t) IRQ_HANDLED;
 }
 
-static const struct of_device_id __test_int_driver_id[] = {
-	{ .compatible = "altr,fpga_interrupts" },
-	{}
+static int __fpga_interrupts_driver_probe(struct platform_device* pdev)
+{
+   int ret;
+   int irq_num;
+   int i = 0;
+
+   ret = device_create_file(&pdev->dev, &dev_attr_irq_status);
+   if (ret) {
+      printk(KERN_ALERT "%s: Error %d when executing device_create_file in function %s!\n", DEVNAME, ret, __FUNCTION__);
+      return ret;
+   }
+
+   while((irq_num = platform_get_irq_optional(pdev, i)) > 0) {
+      printk(KERN_INFO "%s: IRQ %d about to be registered!\n", DEVNAME, irq_num);
+      ret = request_irq(irq_num, (irq_handler_t) __fpga_interrupts_driver_isr, 0, DEVNAME, &pdev->dev.kobj);
+      if (ret < 0) {
+         printk(KERN_ALERT "%s: Error %d when executing request_irq (irq_num=%d) in function %s!\n", DEVNAME, ret, irq_num, __FUNCTION__);
+         return ret;
+      }
+      irq_nums[i] = irq_num;
+      i++;
+   }
+
+   return 0;
+}
+
+static int __fpga_interrupts_driver_remove(struct platform_device* pdev)
+{
+   int irq_num;
+
+   int i = 0;
+   while((irq_num = platform_get_irq_optional(pdev, i)) > 0) {
+      printk(KERN_INFO "%s: IRQ %d about to be freed!\n", DEVNAME, irq_num);
+      free_irq(irq_num, &pdev->dev.kobj);
+      irq_nums[i] = 0;
+      i++;
+   }
+
+   device_remove_file(&pdev->dev, &dev_attr_irq_status);
+
+   return 0;
+}
+
+static const struct of_device_id __fpga_interrupts_driver_id[] = {
+   {.compatible = "altr,fpga_interrupts"},
+   {}
 };
 
-static const struct platform_driver __test_int_driver = {
-	.driver = {
-		.name = DEV_NAME,
-		.owner = THIS_MODULE,
-		.of_match_table = of_match_ptr(__test_int_driver_id),
-	},
-	.probe = __test_int_driver_probe,
-	.remove = __test_int_driver_remove,
+static struct platform_driver __fpga_interrupts_driver = {
+   .driver = {
+      .name = DEVNAME,
+      .owner = THIS_MODULE,
+      .of_match_table = of_match_ptr(__fpga_interrupts_driver_id),
+   },
+   .probe = __fpga_interrupts_driver_probe,
+   .remove = __fpga_interrupts_driver_remove
 };
 
-module_platform_driver(__test_int_driver);
+module_platform_driver(__fpga_interrupts_driver);
 
 MODULE_LICENSE("GPL");
